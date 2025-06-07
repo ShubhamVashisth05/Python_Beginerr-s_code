df8d8f0d0c6ad102f54304626460498fff573f6c
Shubham Vashisth <shubham70555@gmail.com>
2025-06-07 19:53:13 +0530

    Add files via upload

---
+++ word_replacement_app.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Hi guys, Welcome to my yt channel Shibhhit"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the replacement word: ")
+    print(str.replace(word_to_replace,word_replacement))
+
+replace_word()
